4f817e24e4b110f3cc80be6c4272268208c99b26
airi0623 <marcy.0623.doi@gmail.com>
2020-09-25 21:45:17 +0900

    Things model 変更

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,12 +1,38 @@
 class Thing < ActiveHash::Base
   self.data = [
-    { id: 1, name: '材料を選択してください', unit: '自動で入力されます'},
-    { id: 2, name: 'じゃがいも', unit: '個' },
-    { id: 3, name: 'にんじん' , unit: '本'},
-    { id: 4, name: 'ほうれん草' , unit: '束'},
-    { id: 5, name: '玉ねぎ' , unit: '個'},
-    { id: 6, name: 'なすび' , unit: '個'},
-    { id: 7, name: 'カレーのルー' , unit: '個'},
-    { id: 8, name: '水' , unit: 'ml'},
+    { id: 0, name: '材料を選択してください'},
+    # 個
+    { id: 1, name: 'じゃがいも'},
+    { id: 2, name: '玉ねぎ'},
+    { id: 3, name: 'なすび' },
+    { id: 4, name: 'ピーマン' },
+    { id: 5, name: 'しいたけ' },
+    { id: 6, name: 'お好みのルー' },
+
+    # 本
+    { id: 101, name: 'にんじん' },
+    { id: 102, name: 'ねぎ' },
+    { id: 103, name: '大根' },
+
+    # 束
+    { id: 201, name: 'ほうれん草' },
+    { id: 202, name: 'ニラ' },
+
+    # ml
+    { id: 301, name: '水' },
+    { id: 302, name: '醤油' },
+    { id: 303, name: 'みりん' },
+    { id: 304, name: '酒' },
+    { id: 305, name: 'ごま油' },
+
+    # 袋
+    { id: 401, name: 'もやし' },
+    { id: 402, name: 'しらたき' },
+
+    # g
+    { id: 501, name: '砂糖' },
+    { id: 502, name: '豚肉(細切れ)' },
+    { id: 503, name: '鶏(胸肉)' },
+    { id: 504, name: '鶏(もも肉)' },
   ]
 end
